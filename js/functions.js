fbad95eb3fa06bb2a50efd0f0529c379d03241ba
Pim De Witte <p.dwitte@soulsplit.com>
2013-11-14 16:52:20 -0500

    Updated with water and food

--- js/functions.js
+++ js/functions.js
@@ -7,7 +7,7 @@ var app = {
     app.view.initialize();
 
         if(pagetype == 0){
-            document.getElementById("container").innerHTML = '[EVENT_TYPE,LatLng(9.7874,120363.53),PERSON_NAME,0,CONTACT_INFORMATION,MORE_INFORMATION]';
+           // document.getElementById("container").innerHTML = '[EVENT_TYPE,LAT,LEN,PERSON_NAME,0,CONTACT_INFORMATION,MORE_INFORMATION]';
         }
 
     }
@@ -44,7 +44,7 @@ function openInputArea(latlng, type) {
     if(currentPos === null) {
         alert("Please select a location by clicking somewhere on the map first. Make sure you are zoomed in enough.");
     } else {
-
+    document.getElementById("geolocation").value = currentPos;
     transform(document.getElementById("fullform"), "display", "block");
     transform(document.getElementById("page2"), "display", "none");
     transform(document.getElementById("page1"), "display", "block");
@@ -57,11 +57,12 @@ function closeInputArea() {
 }
 
 function sendDetails() {
+    currentPos = (document.getElementById("geolocation").value);
     sendObject.push(currentPos);
-    sendObject.push(document.getElementById("name").value);
-    sendObject.push(document.getElementById("heads").value);
-    sendObject.push(document.getElementById("contactinfo").value);
-    sendObject.push(document.getElementById("information").value);
+    sendObject.push(encodeURIComponent(document.getElementById("name").value));
+    sendObject.push(encodeURIComponent(document.getElementById("heads").value));
+    sendObject.push(encodeURIComponent(document.getElementById("contactinfo").value));
+    sendObject.push(encodeURIComponent(document.getElementById("information").value));
 
     if(sendObject.length != 6){
         alert("Please fill in all the fields!" + sendObject.length);
@@ -82,49 +83,59 @@ var IOConnection = {
         ioc = io.connect(hostaddress + ':' + port, {'transports': ['xhr-polling']}),
 
             ioc.on('connect', function() {
-                ioc_debug.setStart();
+            ioc_debug.setStart();
 
-                console.log("Succesfully connected to "+hostaddress+":"+port+" in "  + (ioc_debug.start -  ioc_debug.end) + " milliseconds");
+            console.log("Succesfully connected to "+hostaddress+":"+port+" in "  + (ioc_debug.start -  ioc_debug.end) + " milliseconds");
 
-            });
+        });
+
+        ioc.on('count', function(data) {
+        console.log(data);
+
+        });
             ioc.on('addloc', function(data) {
-                //data = urldecode(data);
+
                 var ar = data.split(",");
+
 
                 if(filter != null && filter.length > 2) {
                     if(ar[1].indexOf(filter) === -1){
+                        document.getElementById("filter").innerHTML = "Reset Filter";
+                        document.getElementById("filterurl").href = "/";
                         console.log("blocking filter");
                         return;
                     }
                 }
 
                 if(latlens[[[ar[2]],ar[3]]] === true){
-                    console.log("BLOCK:"+[ar[2]],ar[3]);
+                    console.log("BLOCK:"+[ar[2]],ar[3]+":"+ar[1]);
                     return;
                 }
                 latlens[[[ar[2]],ar[3]]] = true;
 
-                if(ar[2] != 100 && ar[3] != 100 && ar.length === 8 && ar[6].length > 2){
+                if(ar[2] != 100 && ar[3] != 100 && ar.length === 8){
+                    console.log(data);
                 if(pagetype == 1){
                 L.marker([[ar[2]],ar[3]]).addTo(map)
                     .bindPopup(
-                        "<br/><b>Type:</b>  " + ar[1] +
-                        "<br/><b>Name:</b>  " + ar[4] +
-                        "<br/><b>Info:</b><br/> " + ar[5] +
-                        "<br/><b>Contact:</b><br/>  " + ar[6] +
-                        "<br/><b>Amount of people:</b> " + ar[7]
+                        "CID: "+ar[0]+"<br/><b>Type:</b> <a target='_blank' href='/#"+ urldecode(ar[1]) +"'>" + urldecode(ar[1]) +
+                        "</a><br/><b>IMAGE:</b><a target='_blank' href='"+ urldecode(ar[4]) +"'><img src='" + urldecode(ar[4]) +
+                        "' style='width:100px;'/></a><br/><b>Info:</b><br/> " + urldecode(ar[5]) +
+                        "<br/><b>Contact:</b><br/>  " + urldecode(ar[6]) +
+                        "<br/><b>Amount of people:</b> " + urldecode(ar[7])
                     )
                     .openPopup();
                 }else{
                     if(pagetype == 0){
-
-                    document.getElementById("container").innerHTML +=  "<br/><a target='_blank' href='/#"+ar[1]+"'>[" + ar[1] +
+                    document.getElementById("container").innerHTML +=  "[" +
+                        ar[0] +
+                        "," + ar[1] +
                         "," + ar[2] +
                         "," + ar[3] +
                         "," + ar[4] +
                         "," + ar[5] +
                         "," + ar[6] +
-                        "," + ar[7] + "]</a>";
+                        "," + ar[7] + "],";
                 }else if(pagetype == 2){
                         if(document.getElementById("container").innerHTML.indexOf(ar[1]) == -1){
                         document.getElementById("container").innerHTML +=  "<br/><a target='_blank' href='/#"+ar[1]+"'>" + ar[1] +"</a>";}
